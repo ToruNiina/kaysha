affec5df603fcef050b43bd81862c1959a68fef4
ToruNiina <niina.toru.68u@gmail.com>
2019-06-02 22:36:11 +0900

    :sparkles: add differentiation

---
+++ kaysha/differentiation.hpp
@@ -0,0 +1,9 @@
+#ifndef KAYSHA_DIFFERENTIATION_HPP
+#define KAYSHA_DIFFERENTIATION_HPP
+
+namespace kaysha
+{
+template<typename T>
+struct differentiation;
+} // kaysha
+#endif// KAYSHA_DIFFERENTIATION_HPP
