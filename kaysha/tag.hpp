83fe75d66cd5fdf12b9cd0ee4e0d74d4cc85a1ae
ToruNiina <niina.toru.68u@gmail.com>
2019-06-02 22:35:21 +0900

    :sparkles: add tag type to distinguish kaysha types

---
+++ kaysha/tag.hpp
@@ -0,0 +1,12 @@
+#ifndef KAYSHA_TAG_HPP
+#define KAYSHA_TAG_HPP
+#include <type_traits>
+
+namespace kaysha
+{
+struct kaysha_type{};
+
+template<typename T>
+struct is_kaysha_type public: std::is_base_of<kaysha_type, T> {};
+} // kaysha
+#endif// KAYSHA_TAG_HPP
