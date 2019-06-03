f3f917f4c9efe8ece4adfe378447eefae755d35b
ToruNiina <niina.toru.68u@gmail.com>
2019-06-04 00:24:09 +0900

    :sparkles: add differentiate() function

--- kaysha/differentiation.hpp
+++ kaysha/differentiation.hpp
@@ -3,11 +3,18 @@
 
 namespace kaysha
 {
+
 template<typename T>
 struct differentiation;
 
 template<typename T>
 using differentiation_t = typename differentiation<T>::type;
+
+template<typename T>
+constexpr inline differentiation_t<T> differentiate(const T& f) noexcept
+{
+    return differentiation<T>::make(f);
+}
 
 } // kaysha
 #endif// KAYSHA_DIFFERENTIATION_HPP
