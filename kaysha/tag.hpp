e943956528cf99f579c912bd676d9ecc1932506c
ToruNiina <niina.toru.68u@gmail.com>
2019-06-04 00:10:11 +0900

    :pencil2: fix typo

--- kaysha/tag.hpp
+++ kaysha/tag.hpp
@@ -7,6 +7,6 @@ namespace kaysha
 struct kaysha_type{};
 
 template<typename T>
-struct is_kaysha_type public: std::is_base_of<kaysha_type, T> {};
+struct is_kaysha_type: public std::is_base_of<kaysha_type, T> {};
 } // kaysha
 #endif// KAYSHA_TAG_HPP
