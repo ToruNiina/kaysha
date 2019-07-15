b9467c93aed987ffa4906068f68804f92c033ba7
ToruNiina <niina.toru.68u@gmail.com>
2019-07-15 22:08:09 +0900

    :art: rename template argument and member variable

--- kaysha/power.hpp
+++ kaysha/power.hpp
@@ -29,12 +29,12 @@ struct power_impl<Real, 0>
 };
 } // detail
 
-template<typename Lhs, std::int64_t N>
-struct power_of: public kaysha_type<typename Lhs::value_type>
+template<typename Term, std::int64_t N>
+struct power_of: public kaysha_type<typename Term::value_type>
 {
-    using value_type = typename Lhs::value_type;
+    using value_type = typename Term::value_type;
 
-    constexpr power_of(const Lhs& l) noexcept: lhs(l) {}
+    constexpr power_of(const Term& t) noexcept: term(t) {}
     constexpr power_of(power_of const&) noexcept = default;
     constexpr power_of(power_of &&)     noexcept = default;
     power_of& operator=(power_of const&) noexcept = default;
@@ -43,33 +43,33 @@ struct power_of: public kaysha_type<typename Lhs::value_type>
 
     constexpr value_type operator()(value_type x) const noexcept
     {
-        return N >= 0 ? detail::power_impl<value_type, N>::invoke(lhs(x)) :
-            value_type(1) / detail::power_impl<value_type, N>::invoke(lhs(x));
+        return N >= 0 ? detail::power_impl<value_type, N>::invoke(term(x)) :
+            value_type(1) / detail::power_impl<value_type, N>::invoke(term(x));
     }
-    Lhs lhs;
+    Term term;
 };
 
-template<typename Lhs, std::int64_t N>
-struct differentiation<power_of<Lhs, N>>
+template<typename Term, std::int64_t N>
+struct differentiation<power_of<Term, N>>
 {
-    using value_type = typename Lhs::value_type;
+    using value_type = typename Term::value_type;
 
-    using antiderivative = power_of<Lhs, N>;
-    using type = multiplication<constant<value_type>, power_of<Lhs, N-1>>;
+    using antiderivative = power_of<Term, N>;
+    using type = multiplication<constant<value_type>, power_of<Term, N-1>>;
 
     static constexpr type make(const antiderivative& ad) noexcept
     {
         return type(constant<value_type>(value_type(N)),
-                    power_of<Lhs, N-1>(ad.lhs));
+                    power_of<Term, N-1>(ad.term));
     }
 };
 
-template<typename Lhs>
-struct differentiation<power_of<Lhs, 0>>
+template<typename Term>
+struct differentiation<power_of<Term, 0>>
 {
-    using value_type = typename Lhs::value_type;
+    using value_type = typename Term::value_type;
 
-    using antiderivative = power_of<Lhs, 0>;
+    using antiderivative = power_of<Term, 0>;
     using type = zero<value_type>;
 
     static constexpr type make(const antiderivative& ad) noexcept
@@ -79,13 +79,13 @@ struct differentiation<power_of<Lhs, 0>>
 };
 
 
-template<std::int64_t N, typename Lhs>
+template<std::int64_t N, typename Term>
 constexpr typename std::enable_if<
-    is_kaysha_type<Lhs>::value, power_of<Lhs, N>
+    is_kaysha_type<Term>::value, power_of<Term, N>
     >::type
-power(const Lhs& l) noexcept
+power(const Term& t) noexcept
 {
-    return power_of<Lhs, N>(l);
+    return power_of<Term, N>(t);
 }
 
 } // kaysha
